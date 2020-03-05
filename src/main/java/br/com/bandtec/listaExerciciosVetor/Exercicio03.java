98ddb83019ccc0b6e3a713422bd9c9860a8ba3a3
fernandoguide <fernando-guide@hotmail.com>
2020-03-05 20:44:04 -0300

    att

--- src/main/java/br/com/bandtec/listaExerciciosVetor/Exercicio03.java
+++ src/main/java/br/com/bandtec/listaExerciciosVetor/Exercicio03.java
@@ -6,8 +6,7 @@ public class Exercicio03 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[] vet = new int[10];
-        int soma = 0;
-
+        double soma = 0.0;
         for (int i = 0; i < 10; i++) {
             System.out.println("Digite um numero: ");
             vet[i] = sc.nextInt();
@@ -16,8 +15,13 @@ public class Exercicio03 {
             soma += valor;
         }
         System.out.println("Total do vetor= " + soma);
-        int media = soma / 10;
+        double media = soma / 10;
         System.out.println("Media do vetor= " + media);
-
+        System.out.println("Maiores que a media:");
+        for (int i = 0; i < vet.length; i++) {
+            if (vet[i] > media) {
+                System.out.print("{" + vet[i]+"}\t");
+            }
+        }
     }
 }
